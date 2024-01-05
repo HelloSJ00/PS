c24fe22c4d18f6bbba30fbb51e311b9f59ffeb5b
HELLOSJ00 <skku03062@g.skku.edu>
2024-01-05 22:43:12 +0900

    24/01/05 PS

--- BOJ/DP/2156/2156.py
+++ BOJ/DP/2156/2156.py
@@ -2,9 +2,16 @@ import sys
 input = sys.stdin.readline
 
 N = int(input()) #포도주 잔의 개수
-grapes = []
-for i in range(N):
-    grapes.append(int(input()))
+grapes = [0]*10001
+dp=[0]*10001
 
-def best_combi(list):
+for i in range(N):
+    grapes[i+1]=int(input())
+
+dp[1] = grapes[1]
+dp[2] = grapes[1]+grapes[2]
+for i in range(3,N+1):
+    dp[i] = max(dp[i-1],dp[i-2]+grapes[i],dp[i-3]+grapes[i-1]+grapes[i])
+
+print(dp[N])
 
